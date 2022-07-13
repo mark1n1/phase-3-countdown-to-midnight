0df88722fd7c97bbc084fa69e66c7fecf5e7ea57
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-13 16:20:57 -0700

    Completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(countdown)
+  while countdown != 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown)
+  while countdown != 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
